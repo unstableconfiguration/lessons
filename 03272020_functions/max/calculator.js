1f5d2df6c27f85444f08c12398e7279827171abd
muffins <mluper@protonmail.com>
2020-04-01 15:40:51 -0400

    early commmit to check markdown formatting

--- 03272020_functions/max/calculator.js
+++ 03272020_functions/max/calculator.js
@@ -1,23 +1,24 @@
 /* Indention: 
-    As with the HTML, whenever we are within a given scopee, we add 
-    indentation. In .js, instead of it being within <tags>, it is whenever 
-    we are within {braces}. 
+    As with the HTML, and with most languages, whenever we are within a given scope, 
+    we add indentation. In .js, instead of it being within <tags>, it is whenever 
+    we are within {
+        braces
+    }. 
 */
 
 let buttonAdd = document.getElementById('buttonAdd');
 let buttonSubtract = document.getElementById('buttonSubtract');
 let output = document.getElementById('output');
 
-/* Style point: functions named after verbs 
+/* Style point: verb function names
     Functions do work. You know that you have a well-crafted function when the work 
-    it does can be expressed succinctly in its name. There are some conventions in which 
-    you indicate in the name that it is a function, but for us right now, it is good to keep 
-    the name short and sweet and based entirely on what it is doing.  
+    it does can be expressed succinctly in its name. In a very simple function, this 
+    could be as simple as a single verb (add, subtract). In more complex functions, you can 
+    still base the name around a verb (sendEmails, cancelRequest).  
 */
 let add = function(a, b){
     /* What is happening here is called 'value coercion', and is an important 
-    part of javascript. I'm going to write more about it in valueCoercion.js. Basically though, 
-    when we add the + in front of our values, it is effectively the same as doing parseInt() */
+    part of javascript. I'll cover this in another document. */
     return +a + +b;
 };
 
@@ -25,12 +26,6 @@ let subtract = function(a, b){
     /* The minus sign automatically coerces the values for us! Again, more of that in another file. */
     return a - b;
 };
-
-let logOutput = function(value) { 
-    /* An example of a bonus function. We'll cover the whys of this another time since I want to talk 
-    about value coercions and don't want to overload with information.  */
-    output.value = value;
-}
 
 /* Naming scheme deviation: event handlers 
     Above, I talked about naming functions after what they do. One exception to this is event handlers. 
@@ -41,11 +36,11 @@ let logOutput = function(value) {
 */
 let onButtonAddClick = function() { 
     let value = add(leftNumber.value, rightNumber.value);
-    logOutput(value);
+    output.value = value;
 }
 
 let onButtonSubtractClick = function() { 
-    logOutput(subtract(leftNumber.value, rightNumber.value));
+    output.value = subtract(leftNumber.value, rightNumber.value);
 }
 
 buttonAdd.addEventListener('click', onButtonAddClick);
---
+++ 04012020_arrays_and_flow_control/lesson.md
@@ -0,0 +1,18 @@
+## Arrays and Flow Control
+In many tutorials, you start with data types into flow control into arrays, then into 
+functions and classes. I wanted to actully push functions first since I consider them 
+more complex, and then work backwards for a couple lessons to let them sink in.  
+
+### Arrays  
+Arrays are a data type in Javascript and other languages. What they do is hold a collection 
+of other values. For example, an array of integers might look like this: 
+```
+let numberArray = [1, 2, 5, 3];
+```
+Items in an array are accessed by their 'index', or their position in the array. Arrays in 
+.js start at 0, so we can access the first value with the 0 index, the second with the 1 index 
+and so on.
+```
+let one = numberArray[0];
+let five = numberArray[3];  
+```
