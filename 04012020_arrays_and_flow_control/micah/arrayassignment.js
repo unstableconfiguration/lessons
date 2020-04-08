10df04fdd96594b788097019a7f5282fc65d1988
digimicah <digimicah@hotmail.com>
2020-04-08 07:55:36 -0400

    Update arrayassignment.js
    Added last two functions.

--- 04012020_arrays_and_flow_control/micah/arrayassignment.js
+++ 04012020_arrays_and_flow_control/micah/arrayassignment.js
@@ -9,10 +9,34 @@ let vowelFinder = function(source){
     return returnedVowels;
 };
 
-let testString = "test string";
-let testArray = ['a', 'b', 'c', 'd', 'e'];
+//let testString = "test string";
+//let testArray = ['a', 'b', 'c', 'd', 'e'];
 
-let returnedString = vowelFinder(testString);
-console.log(returnedString);
-let returnedArray = vowelFinder(testArray);
-console.log(returnedArray);
+//let returnedString = vowelFinder(testString);
+//console.log(returnedString);
+//let returnedArray = vowelFinder(testArray);
+//console.log(returnedArray);
+
+let itemCounter = function(source, term) {
+    let itemCount = 0;
+    for(let i = 0; i <= source.length-1; i++){
+        if (source[i] == term){
+            itemCount++;
+        }
+    }
+    return itemCount;
+};
+
+//let x = itemCounter(testString, 't');
+//console.log(x);
+//let y = itemCounter(testArray, 'b');
+//console.log(y);
+
+let vowelCounter = function(source){
+    let vowelArray = vowelFinder(source);
+    let vowelCount = itemCounter(vowelArray, 'a') + itemCounter(vowelArray, 'e') + itemCounter(vowelArray, 'i') + itemCounter(vowelArray, 'o') + itemCounter(vowelArray, 'u');
+    return vowelCount;
+};
+
+//let z = vowelCounter(testString);
+//console.log(z);
