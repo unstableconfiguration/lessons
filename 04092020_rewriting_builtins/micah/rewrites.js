d23a463d2a78b029273577eeb2337d0533e10d2e
digimicah <digimicah@hotmail.com>
2020-04-16 14:55:00 -0400

    Update rewrites.js
    Added sum()

--- 04092020_rewriting_builtins/micah/rewrites.js
+++ 04092020_rewriting_builtins/micah/rewrites.js
@@ -18,12 +18,19 @@ let find = function(array, callback) {
     return false;
 }
 
-
+let sum = function(array) {
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+        total = total + array[i];
+    }
+    return total;
+}
 
 
 //for testing purposes
    
-//const array1 = [120, 4, 2, 3];
+const array1 = [120, 4, 2, 3];
 //const isBelowThreshold = (currentValue) => currentValue < 40;
 //console.log(every(array1, isBelowThreshold));
 //console.log(find(array1, isBelowThreshold));
+console.log(sum(array1));
