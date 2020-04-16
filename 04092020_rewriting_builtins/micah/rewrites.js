e47e20103968c9c08a01e02e3b772147d63fe20d
digimicah <digimicah@hotmail.com>
2020-04-16 15:03:36 -0400

    Update rewrites.js
    Added final filter()

--- 04092020_rewriting_builtins/micah/rewrites.js
+++ 04092020_rewriting_builtins/micah/rewrites.js
@@ -26,11 +26,21 @@ let sum = function(array) {
     return total;
 }
 
+let filter = function(array, callback) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i]) == true) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
 
 //for testing purposes
    
-const array1 = [120, 4, 2, 3];
+//const array1 = [120, 4, 2, 3];
 //const isBelowThreshold = (currentValue) => currentValue < 40;
 //console.log(every(array1, isBelowThreshold));
 //console.log(find(array1, isBelowThreshold));
-console.log(sum(array1));
+//console.log(sum(array1));
+//console.log(filter(array1, isBelowThreshold));
