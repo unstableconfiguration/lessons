86474fee22e948a2b7ad859c90420f44ed0ba043
digimicah <digimicah@hotmail.com>
2020-04-16 14:49:28 -0400

    Update rewrites.js
    Added find()

--- 04092020_rewriting_builtins/micah/rewrites.js
+++ 04092020_rewriting_builtins/micah/rewrites.js
@@ -7,9 +7,23 @@ let every = function(array, callback) {
     }
     return true;
 }
+
+let find = function(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        if (callback(array[i]) == true) {
+            return array[i];
+            break;
+        }
+    }
+    return false;
+}
+
+
+
+
 //for testing purposes
-/*   
-const array1 = [12, 4, 2, 3];
-const isBelowThreshold = (currentValue) => currentValue < 40;
-console.log(every(array1, isBelowThreshold));
-*/
+   
+//const array1 = [120, 4, 2, 3];
+//const isBelowThreshold = (currentValue) => currentValue < 40;
+//console.log(every(array1, isBelowThreshold));
+//console.log(find(array1, isBelowThreshold));
