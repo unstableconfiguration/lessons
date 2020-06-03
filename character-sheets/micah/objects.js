49d89c6442db42831cb71c80b224c0d2e111b113
digimicah <digimicah@hotmail.com>
2020-06-03 17:40:18 -0400

    Skill and meta pass
    Linked prof bonuses and some more character detail stats.

--- character-sheets/micah/objects.js
+++ character-sheets/micah/objects.js
@@ -15,6 +15,34 @@ let conModDisplay = document.getElementById('conmod');
 let intModDisplay = document.getElementById('intmod');
 let wisModDisplay = document.getElementById('wismod');
 let chaModDisplay = document.getElementById('chamod');
+let hpDisplay = document.getElementById('hitpoints');
+let profBonusDisplay = document.getElementById('profbonus');
+let moveSpeedDisplay = document.getElementById('movespeed');
+let initiativeDisplay = document.getElementById('initiative');
+let armorClassDisplay = document.getElementById('armorclass');
+let acroProfModDisplay = document.getElementById('acroprofmod');
+let animProfModDisplay = document.getElementById('animprofmod');
+let arcaProfModDisplay = document.getElementById('arcaprofmod');
+let athlProfModDisplay = document.getElementById('athlprofmod');
+let deceProfModDisplay = document.getElementById('deceprofmod');
+let histProfModDisplay = document.getElementById('histprofmod');
+let insiProfModDisplay = document.getElementById('insiprofmod');
+let intiProfModDisplay = document.getElementById('intiprofmod');
+let inveProfModDisplay = document.getElementById('inveprofmod');
+let mediProfModDisplay = document.getElementById('mediprofmod');
+let natuProfModDisplay = document.getElementById('natuprofmod');
+let percProfModDisplay = document.getElementById('percprofmod');
+let perfProfModDisplay = document.getElementById('perfprofmod');
+let persProfModDisplay = document.getElementById('persprofmod');
+let reliProfModDisplay = document.getElementById('reliprofmod');
+let sleiProfModDisplay = document.getElementById('sleiprofmod');
+let steaProfModDisplay = document.getElementById('steaprofmod');
+let survProfModDisplay = document.getElementById('survprofmod');
+
+
+
+
+
 
 let yorim = {
     name : 'Yorim',
@@ -22,6 +50,8 @@ let yorim = {
     alignment : 'Chaotic Good',
     level : 8,
     class : 'Rogue',
+    movespeed : 35,
+    ac : 17,
     stats : {
         str : 10,
         dex : 20,
@@ -29,6 +59,7 @@ let yorim = {
         int : 13,
         wis : 14,
         cha : 10,
+    
     },
 
     statMod : function(stat) {
@@ -62,6 +93,30 @@ conModDisplay.innerHTML = yorim.statMod(yorim.stats.con);
 intModDisplay.innerHTML = yorim.statMod(yorim.stats.int);
 wisModDisplay.innerHTML = yorim.statMod(yorim.stats.wis);
 chaModDisplay.innerHTML = yorim.statMod(yorim.stats.cha);
+hpDisplay.innerHTML = yorim.hpCalc();
+profBonusDisplay.innerHTML = yorim.profMod();
+moveSpeedDisplay.innerHTML = yorim.movespeed;
+initiativeDisplay.innerHTML = yorim.statMod(yorim.stats.dex);
+armorClassDisplay.innerHTML = yorim.ac;
+acroProfModDisplay.innerHTML = yorim.statMod(yorim.stats.dex);
+animProfModDisplay.innerHTML = yorim.statMod(yorim.stats.wis);
+arcaProfModDisplay.innerHTML = yorim.statMod(yorim.stats.int);
+athlProfModDisplay.innerHTML = yorim.statMod(yorim.stats.str);
+deceProfModDisplay.innerHTML = yorim.statMod(yorim.stats.cha);
+histProfModDisplay.innerHTML = yorim.statMod(yorim.stats.int);
+insiProfModDisplay.innerHTML = yorim.statMod(yorim.stats.wis);
+intiProfModDisplay.innerHTML = yorim.statMod(yorim.stats.cha);
+inveProfModDisplay.innerHTML = yorim.statMod(yorim.stats.int);
+mediProfModDisplay.innerHTML = yorim.statMod(yorim.stats.wis);
+natuProfModDisplay.innerHTML = yorim.statMod(yorim.stats.int);
+percProfModDisplay.innerHTML = yorim.statMod(yorim.stats.wis);
+perfProfModDisplay.innerHTML = yorim.statMod(yorim.stats.cha);
+persProfModDisplay.innerHTML = yorim.statMod(yorim.stats.cha);
+reliProfModDisplay.innerHTML = yorim.statMod(yorim.stats.int);
+sleiProfModDisplay.innerHTML = yorim.statMod(yorim.stats.dex);
+steaProfModDisplay.innerHTML = yorim.statMod(yorim.stats.dex);
+survProfModDisplay.innerHTML = yorim.statMod(yorim.stats.wis);
+
 
 //console.log(yorim.statMod(yorim.stats.dex));
 //console.log(yorim.hpCalc());
